ef9b4808a36f4c31e3bf7fcf867a95283ab5b2fb
alexandre charmoso <alexandregaia911@gmail.com>
2023-12-15 21:39:00 -0300

    finalizing the project

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 rounded-4 text-center">
+      <h1>Arithmetics Calculation</h1>
+      <div class="text-center mb-3">
+        <select v-model="selectedOperation" class="form-select-sm" @change="resetAndCalculate">
+          <option value="" disabled>Select the Operation</option>
+          <option value="addition">Addition</option>
+          <option value="subtraction">Subtraction</option>
+          <option value="multiplication">Multiplication</option>
+          <option value="division">Division</option>
+        </select>
+      </div>
+    </header>
+
+    <Formulas :resetResult="resetResult" :selectedOperation="selectedOperation" />
+  </div>
+</template>
+
+<script setup>
+import Formulas from './Components/Formulas.vue';
+import { ref } from 'vue';
+
+const selectedOperation = ref('');
+
+const resetResult = () => {
+};
+
+const resetAndCalculate = () => {
+  resetResult();
+};
+
+</script>
+
+<style>
+body {
+  background-color: #eeecec;
+}
+
+.result {
+  max-width: 70px;
+}
+</style>
---
+++ src/Components/Formulas.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <label for="1st">Insert the first number:</label>
+        <input v-model="firstNumber" @input="calculateResult" @change="calculateResult" class="ms-3" type="number">
+
+        <label class="ms-3" for="2nd">Insert the second number:</label>
+        <input v-model="secondNumber" @input="calculateResult" @change="calculateResult" class="ms-3" type="number">
+
+        <span> = </span>
+
+        <input v-model="result" class="result ms-1" readonly>
+    </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+const props = defineProps(['resetResult', 'selectedOperation']);
+
+
+const firstNumber = ref(0);
+const secondNumber = ref(0);
+const result = ref(0);
+
+const calculateResult = () => {
+    const num1 = parseFloat(firstNumber.value);
+    const num2 = parseFloat(secondNumber.value);
+    console.log('selectedOperation:', props.selectedOperation);
+
+    switch (props.selectedOperation) {
+        case 'addition':
+            result.value = num1 + num2;
+            break;
+        case 'subtraction':
+            result.value = num1 - num2;
+            break;
+        case 'multiplication':
+            result.value = num1 * num2;
+            break;
+        case 'division':
+            result.value = num2 !== 0 ? num1 / num2 : 'Invalid';
+            break;
+        default:
+            result.value = '';
+    }
+
+
+};
+
+watch(() => props.selectedOperation, (newValue) => {
+    console.log('selectedOperation changed:', newValue);
+});
+
+</script>
